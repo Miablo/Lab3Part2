99f918f6688879de126adf4f9ed84ce3451b2016
miablo <mvd5044@psu.edu>
2021-02-04 17:11:05 -0500

    added instructions

---
+++ src/Findextremes.java
@@ -0,0 +1,15 @@
+public class Findextremes {
+
+    /*
+    (5 pts) Apply the Newton-Raphson's method xn+1 = xn – f '(xn) / f ''(xn) to develop a function double findMinMax(…)
+    that returns a solution that is either a minimum value or a maximum value for z.
+    The program finds a local minimum or a local maximum when the difference between the new solution and
+    the previous one is smaller than 0.00001 within 10000 iterations. Otherwise, it shows Infinity as output.
+
+    Let function z = x4+2y6 – xy – x + 2. Show the post-conditions for z.
+    The preconditions for x and y are shown as below. Feel free to choose an arbitrary initial guess values x0 and
+    y0 that meet the following precondition.
+
+    |x| ≤ 6 and |y| ≤ 5
+     */
+}
