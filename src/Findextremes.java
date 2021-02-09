c2091e8d018d904a1e2278176c56ab2575f3ee4b
Pcow9 <71295830+Pcow9@users.noreply.github.com>
2021-02-09 16:03:30 -0500

    Add math formulas and start of main. need to check derivates for both x and y to verify it is correct.

--- src/Findextremes.java
+++ src/Findextremes.java
@@ -1,5 +1,60 @@
 public class Findextremes {
+    double x;
+    double y;
 
+    /**
+     *
+     * @return math for first derivative of x with current x and y
+     */
+    private double firstDerivativeX(){
+        return (4*(Math.pow(this.x, 3)) - this.y -1.0);
+    }
+
+    /**
+     *
+     * @return math for second derivative of x with current x and y
+     */
+    private double secondDerivativeX(){
+        return (12 * (Math.pow(this.x, 2)));
+    }
+
+    /**
+     *
+     * @return math for first derivative of Y with current x and y
+     */
+    private double firstDerivativeY(){
+        return (-this.x - 12 * (Math.pow(this.y, 5)));
+    }
+
+    /**
+     *
+     * @return math for second derivative of Y with current x and y
+     */
+    private double secondDerivativeY(){
+        return (60 * (Math.pow(this.y, 4)));
+    }
+
+    /**
+     *
+     * @param x = Xn
+     * @param y = Yn
+     * @return next X or Xn+1
+     */
+    public double nextXMath(double x, double y){
+        this.x = x;
+        this.y = y;
+        return(this.x - (firstDerivativeX() / secondDerivativeX()));
+    }
+
+    /**
+     *
+     * @param x = next Xn+1 for Yn
+     * @return next Y or Yn+1
+     */
+    public double nextYMath(double x){
+        this.x = x;
+        return (this.y - (firstDerivativeY() / secondDerivativeY()));
+    }
     /*
     (5 pts) Apply the Newton-Raphson's method xn+1 = xn – f '(xn) / f ''(xn) to develop a function double findMinMax(…)
     that returns a solution that is either a minimum value or a maximum value for z.
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,41 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
+    static Random rand = new Random();
+    static ArrayList<Double> x = new ArrayList();
+    static ArrayList<Double> y = new ArrayList();
+    static int min = -6; //lets there be a min double for random
+    static int max = 6; //lets there be a max double for random
+    static int end = -1;
+    static Findextremes find = new Findextremes();
+
+    public static void main(String[] args) {
+        double a1,b1, a2, b2;
+        int i = 0; //for x variable
+        int j = 0; //for y variable
+        x.add(min + (rand.nextDouble() * (max - min ))); //gets random double between -6 and 6 and adds it as x0
+        y.add((min + 1) + (rand.nextDouble() * ((max - 1) - (min + 1)))); //gets random double between -5 and 5 and adds it as y0
+        do{
+            x.add(find.nextXMath(x.get(i), y.get(j)));
+            i++;
+            y.add(find.nextYMath(x.get(i)));
+            j++;
+            a1 = (double) x.get(i);
+            b1 = (double) x.get(i - 1);
+            a2 = (double) y.get(j);
+            b2 = (double) y.get(j - 1);
+            if (a1 <= b1 + 0.00001 && a1 >= b1 - 0.00001) { //checks if root is found
+                if(a2 <= b2 + 0.00001 && a2 >= b2 - 0.00001) {
+                    end = 0;
+                }
+            }else if(i == 1001){
+                end = 0;
+            }
+        }while(end != 0);
+        System.out.print(x.get(i) + y.get(j));
+        System.out.print("end");
+    }
 
 }
